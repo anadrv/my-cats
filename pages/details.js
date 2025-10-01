57e24d826abf01e3b7dd2214b1d16365c3f836c5
Ana Luisa <anadrvv@gmail.com>
2025-10-01 20:27:41 -0300

    update

--- pages/details.js
+++ pages/details.js
@@ -114,7 +114,7 @@ if (currentIndex === -1) currentIndex = 0;
 
 function goToCat(index) {
   const nextCatName = catsNames[index];
-  window.location.href = `https://anadrv.github.io/my-cats/pages/details.html?cat=${nextCatName}`;
+  window.location.href = `./details.html?cat=${nextCatName}`;
 
 }
 
