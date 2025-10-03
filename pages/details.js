43c24d288e4b315f7825f2303ff1b0414e9a8f70
Ana Luisa <anadrvv@gmail.com>
2025-10-03 15:54:35 -0300

    aurora images

--- pages/details.js
+++ pages/details.js
@@ -36,6 +36,36 @@ const cats = [
     location: "Lagoa Seca - PB",
     birthdate: "07/12/2015",
     images: [
+      "/images/aurora/aurora-img-01.png",
+      "/images/aurora/aurora-img-03.png",
+      "/images/aurora/aurora-img-04.png",
+      "/images/aurora/aurora-img-05.png"
+    ],
+    description: [
+      "Também conhecida como 'teste de DNA', porque é a cara do que a gente achava que era o pai (tinha dois possíveis gatos na história). Foi a segunda a nascer e a mais rápida.",
+      "Desde pequena é meio nervosa — se você pega no colo, ela se enrosca toda de medo. Mas gosta de carinho, ronrona e te lambe se você deixar. "
+    ],
+    age: "2 years",
+    weight: "4.5 kg",
+    gender: "Female",
+    fur: "Short",
+    eyes: "Green",
+    skills: [
+      { name: "Open kitchen door", rating: 3 },
+      { name: "Climb on places", rating: 5 },
+      { name: "Voice (volume)", rating: 4 },
+      { name: "Affection", rating: 3 },
+      { name: "Special skill", rating: "Super" }
+    ],
+    captionImg02: "Named after Apollo from Percy Jackson"
+  },
+  {
+    name: "BATATINHA",
+    nickname: "O MALANDRO",
+    background: "#E8CAD3",
+    location: "Lagoa Seca - PB",
+    birthdate: "07/12/2015",
+    images: [
       "/images/apolo/apolo-img-01.png",
       "/images/apolo/apolo-img-02.png",
       "/images/apolo/apolo-img-03.png",
